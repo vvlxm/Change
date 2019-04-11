57c14a3007d8fecd4bdff32dd3bf5f4ed932f7ab
vvlxm <812045930@qq.com>
2019-04-11 11:58:35 +0100

    Test

---
+++ Change/Assets/Script/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+        }
+        
+
+    }
+
+}  
